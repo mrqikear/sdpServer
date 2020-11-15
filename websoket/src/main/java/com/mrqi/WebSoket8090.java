3f0fd8bcc94ecdae25f3d8a28e4e4492c27da59a
王茂玲 <wangmaoling@wangmaolingdeMacBook-Pro.local>
2020-11-15 23:51:53 +0800

    初始化提交

--- websoket/src/main/java/com/mrqi/WebSoket8090.java
+++ websoket/src/main/java/com/mrqi/WebSoket8090.java
@@ -1,13 +1,11 @@
-package com.sdp;
+package com.mrqi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebSoket8090 {
-
     public static void main(String[] args) {
         SpringApplication.run(WebSoket8090.class, args);
     }
-
 }
