f55b52bf9703861e0d05169d1a6b006d30384fb8
王茂玲 <wangmaoling@wangmaolingdeMacBook-Pro.local>
2020-12-07 22:40:05 +0800

    mybatis-plus

--- sdbHtttpServer/src/main/java/com/mrqi/controller/superAdmin/testController.java
+++ sdbHtttpServer/src/main/java/com/mrqi/controller/superAdmin/testController.java
@@ -2,6 +2,8 @@ package com.mrqi.controller.superAdmin;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class testController {
@@ -9,7 +11,7 @@ public class testController {
 
     @GetMapping("/superadmin/test")
     public String test(){
-
+        Arrays.asList(1,2,3);
         return "is test springboot ";
     }
 }
